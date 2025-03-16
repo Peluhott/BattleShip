06146138264b2adcb4d88710742a65297493475b
Javier <J_sedanomar@uncg.edu>
2025-03-16 19:04:30 -0400

    change ship points to be stored on this object, added logic to placeship function to change null value of square to 's' when placed, later this will be changed to a picture of the piece of the ship

--- src/gameBoard.js
+++ src/gameBoard.js
@@ -9,7 +9,7 @@ class GameBoard {
     ]; //array of ships
 
     mapOfShips = {  // map of ships to their index, to place on the board
-        'Aircraft Carrier': 0,
+        'Aircraft': 0,
         'Battleship': 1,
         'Cruiser': 2,
         'Submarine': 3,
@@ -23,27 +23,29 @@ class GameBoard {
 
     }
 
-    placeShip(point, ship, orientation) { // its going to recieve a point, check what orientation, iterate for length, and place the ship on the board
+    placeShip(index, point, ship, orientation) { // its going to recieve a point, check what orientation, iterate for length, and place the ship on the board
         if (orientation === 'horizontal') {  // if the orientation is horizontal, the ship will be placed horizontally on the board
-            ship.location[0][0] = point[0]; // sets first point of ship x value
-            ship.location[0][1] = point[1]; // sets first point of ship y value
 
+            ships[index][0][0] = point[0]; // sets first point of ship x value
+            ships[index][0][1] = point[1]; // sets first point of ship y value 
+            grid[point[0]][point[1]] = 'S'; // sets the first point of the ship on the board
             for (let i = 1; i < ship.length; i++) {
-                ship.location[i][0] = point[0] + i; // sets the next point of the ship x value
-                ship.location[i][1] = point[1]; // sets the next point of the ship y value
-
+                ships[index][i][0] = point[0] + i; // sets the next point of the ship x value
+                ships[index][i][1] = point[1]; // sets the next point of the ship y value
+                grid[point[0] + i][point[1]] = 'S'; // sets the next point of the ship on the board
             }
         } else if (orientation === 'vertical') {  // if the orientation is vertical, the ship will be placed vertically on the board
 
-            ship.location[0][0] = point[0]; // sets first point of ship x value
-            ship.location[0][1] = point[1]; // sets first point of ship y value
+            ships[index][0][0] = point[0]; // sets first point of ship x value
+            ships[index][0][1] = point[1]; // sets first point of ship y value
             for (let i = 1; i < ship.length; i++) {
 
-                ship.location[i][0] = point[0]; // sets the next point of the ship x value
-                ship.location[i][1] = point[1] + i; // sets the next point of the ship y value
+                ships[index][i][0] = point[0]; // sets the next point of the ship x value
+                ships[index][i][1] = point[1] + i; // sets the next point of the ship y value
+                grid[point[0]][point[1] + i] = 'S'; // sets the next point of the ship on the board
             }
         }
     }
 
-
-}
+    receiveAttack(point) {
+    }
