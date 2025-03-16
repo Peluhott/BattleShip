d42a853bd069c15ea8c50d217c908e318e1d341f
Javier <J_sedanomar@uncg.edu>
2025-03-16 18:39:53 -0400

    Add grid and filled with null values, Add map for ships so that placeship functions know where to put coordinates, Add array of ships of different size, Add array of missed shots, Add constructor for grid that fills with null and makes it 10 by 10, Add function placeship that checks ship orientation and depending on size adds the coordinates to the ship's location, need to add logic to push the completed ship onto the array, and still need to add other functions such as recieve attack, miss shot

---
+++ src/gameBoard.js
@@ -0,0 +1,49 @@
+class GameBoard {
+    grid = []; //array of the board
+    ships = [
+        new Array(5), // Aircraft Carrier (size 5)
+        new Array(4), // Battleship (size 4)
+        new Array(3), // Cruiser (size 3)
+        new Array(3), // Submarine (size 3)
+        new Array(2)  // Destroyer (size 2)
+    ]; //array of ships
+
+    mapOfShips = {  // map of ships to their index, to place on the board
+        'Aircraft Carrier': 0,
+        'Battleship': 1,
+        'Cruiser': 2,
+        'Submarine': 3,
+        'Destroyer': 4
+    }
+
+    missedShots = []; //array of missed shots
+
+    constructor() {
+        this.grid = Array.from({ length: 10 }, () => Array(10).fill(null)); //creates a 10x10 grid with null values
+
+    }
+
+    placeShip(point, ship, orientation) { // its going to recieve a point, check what orientation, iterate for length, and place the ship on the board
+        if (orientation === 'horizontal') {  // if the orientation is horizontal, the ship will be placed horizontally on the board
+            ship.location[0][0] = point[0]; // sets first point of ship x value
+            ship.location[0][1] = point[1]; // sets first point of ship y value
+
+            for (let i = 1; i < ship.length; i++) {
+                ship.location[i][0] = point[0] + i; // sets the next point of the ship x value
+                ship.location[i][1] = point[1]; // sets the next point of the ship y value
+
+            }
+        } else if (orientation === 'vertical') {  // if the orientation is vertical, the ship will be placed vertically on the board
+
+            ship.location[0][0] = point[0]; // sets first point of ship x value
+            ship.location[0][1] = point[1]; // sets first point of ship y value
+            for (let i = 1; i < ship.length; i++) {
+
+                ship.location[i][0] = point[0]; // sets the next point of the ship x value
+                ship.location[i][1] = point[1] + i; // sets the next point of the ship y value
+            }
+        }
+    }
+
+
+}
