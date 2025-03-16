c5865f76f180df883ccc25b8f84f64f9bb00a629
Javier <J_sedanomar@uncg.edu>
2025-03-16 19:13:37 -0400

    change the logic so that it places a letter on the board that corresponds to the ship, make it easier for the game to find the ship that's been hit when a attack is recieved

--- src/gameBoard.js
+++ src/gameBoard.js
@@ -9,11 +9,11 @@ class GameBoard {
     ]; //array of ships
 
     mapOfShips = {  // map of ships to their index, to place on the board
-        'Aircraft': 0,
-        'Battleship': 1,
-        'Cruiser': 2,
-        'Submarine': 3,
-        'Destroyer': 4
+        'A': 0, //aircraft carrier
+        'B': 1, //battleship
+        'C': 2, // cruiser
+        'S': 3, // submarine
+        'D': 4 // destroyer
     }
 
     missedShots = []; //array of missed shots
@@ -23,16 +23,17 @@ class GameBoard {
 
     }
 
-    placeShip(index, point, ship, orientation) { // its going to recieve a point, check what orientation, iterate for length, and place the ship on the board
+    placeShip(letter, point, ship, orientation) { // its going to recieve a point, check what orientation, iterate for length, and place the ship on the board
+        const index = this.mapOfShips[letter]; // gets the index of the ship
         if (orientation === 'horizontal') {  // if the orientation is horizontal, the ship will be placed horizontally on the board
 
             ships[index][0][0] = point[0]; // sets first point of ship x value
             ships[index][0][1] = point[1]; // sets first point of ship y value 
-            grid[point[0]][point[1]] = 'S'; // sets the first point of the ship on the board
+            grid[point[0]][point[1]] = letter; // sets the first point of the ship on the board
             for (let i = 1; i < ship.length; i++) {
                 ships[index][i][0] = point[0] + i; // sets the next point of the ship x value
                 ships[index][i][1] = point[1]; // sets the next point of the ship y value
-                grid[point[0] + i][point[1]] = 'S'; // sets the next point of the ship on the board
+                grid[point[0] + i][point[1]] = letter; // sets the next point of the ship on the board
             }
         } else if (orientation === 'vertical') {  // if the orientation is vertical, the ship will be placed vertically on the board
 
@@ -42,10 +43,21 @@ class GameBoard {
 
                 ships[index][i][0] = point[0]; // sets the next point of the ship x value
                 ships[index][i][1] = point[1] + i; // sets the next point of the ship y value
-                grid[point[0]][point[1] + i] = 'S'; // sets the next point of the ship on the board
+                grid[point[0]][point[1] + i] = letter; // sets the next point of the ship on the board
             }
         }
     }
 
     receiveAttack(point) {
+        if (grid[point[0]][point[1]] === 'X') { // checks to see if point is already hit
+            return 'Already hit'; // change this later to something else, placeholder for now
+        }
+        else if (grid[point[0]][point[1]] === null) { // checks to see if point is empty
+            grid[point[0]][point[1]] = 'X'; // if it is, it will be a miss
+            missedShots.push(point); // adds the point to the missed shots array
+        }
+        else {
+
+        }
     }
+}
